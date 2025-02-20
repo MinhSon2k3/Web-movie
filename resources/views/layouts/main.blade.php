eea74db10a621935cf2a2c749e6121b5a7348ebc
MinhSon2k3 <118090368+MinhSon2k3@users.noreply.github.com>
2025-02-20 20:53:22 +0700

    2

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,6 +9,7 @@
     <link rel="stylesheet" href="/css/main.css">
     @livewireStyles
 </head>
+
 <body class="font-sans bg-gray-900 text-white">
     <nav class="border-b border-gray-800">
         <div class="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between px-4 py-6">
@@ -15,7 +17,9 @@
                 <li>
                     <a href="{{ route('movies.index') }}">
                         <svg class="w-32" viewBox="0 0 96 24" fill="none">
-                            <path d="M18 4l2 4h-3l-2-4h-2l2 4h-3l-2-4H8l2 4H7L5 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V4h-4z" fill="#fff"/>
+                            <path
+                                d="M18 4l2 4h-3l-2-4h-2l2 4h-3l-2-4H8l2 4H7L5 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V4h-4z"
+                                fill="#fff" />
                         </svg>
                     </a>
                 </li>
@@ -30,11 +34,11 @@
                 </li>
             </ul>
             <div class="flex flex-col md:flex-row items-center">
-                 @livewire('search-dropdown')
+                @livewire('search-dropdown')
 
                 <div class="md:ml-4 mt-3 md:mt-0">
                     <a href="#">
-                        <img src="/img/image1.jpg" alt="avatar" class="rounded-full w-8 h-8">
+                        <img src="/img/sgu.jpg" alt="avatar" class="rounded-full w-8 h-8">
                     </a>
                 </div>
             </div>
@@ -43,10 +47,12 @@
     @yield('content')
     <footer class="border border-t border-gray-800">
         <div class="container mx-auto text-sm px-4 py-6">
-            Powered by <a href="https://www.themoviedb.org/documentation/api" class="underline hover:text-gray-300">TMDb API</a>
+            Powered by <a href="https://www.themoviedb.org/documentation/api" class="underline hover:text-gray-300">TMDb
+                API</a>
         </div>
     </footer>
     @livewireScripts
     @yield('scripts')
 </body>
+
 </html>
